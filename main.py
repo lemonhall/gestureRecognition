04d6fe516de04161e010575e6bc385745053dd1c
“lemonhall” <lemonhall2012@qq.com>
2022-07-13 04:37:53 +0800

    add thread code,and jump frame to reduce cpu useage...to 5~50%

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # import necessary packages for hand gesture recognition project using Python OpenCV
-
 import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+import os
+from threading import Thread
+import sys
+from queue import Queue
+import time
 
 #1、建立环境
 #conda create --name GestureRecognition python=3.8 --channel https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
@@ -27,6 +31,106 @@ from tensorflow.keras.models import load_model
 
 #接下来就是开始编码了
 
+
+#晚上，我修改了第二版本，使用一个单独的线程来做计算和frame上的字体处理啥的
+#延迟已经很低了，可以接受了，但即使是这样cpu还是看到手的时候就飙到90%左右，
+#心累啊，说到底，就是.....哀家需要GPU......
+#行吧行吧，以后得配一个GPU的运算机器啊
+class FileVideoStream:
+	def __init__(self, path, queueSize=128):
+		# initialize the file video stream along with the boolean
+		# used to indicate if the thread should be stopped or not
+		self.stream = cv2.VideoCapture(path,cv2.CAP_FFMPEG)
+		self.stopped = False
+
+		# initialize the queue used to store frames read from
+		# the video file
+		self.Q = Queue(maxsize=queueSize)
+		print("FileVideoStream has been INIT")
+
+	def start(self):
+		# start a thread to read frames from the file video stream
+		t = Thread(target=self.update, args=())
+		print("FileVideoStream has been started")
+		t.daemon = True
+		t.start()
+		return self
+
+	def update(self):
+		counter = 0
+		# keep looping infinitely
+		while True:
+			# if the thread indicator variable is set, stop the thread
+			if self.stopped:
+				print("the thread indicator variable is set, stop the threading")
+				return
+
+			# otherwise, ensure the queue has room in it
+			if not self.Q.full():
+				# read the next frame from the file
+				(grabbed, frame) = self.stream.read()
+
+				# if the `grabbed` boolean is `False`, then we have
+				# reached the end of the video file
+				if not grabbed:
+					print("I am not grabbed.....")
+					self.stop()
+					return
+
+				#用这个跳帧法，基本能做到CPU50%不发烫了，好不容易啊
+				#所以还是需要GPU，有GPU了，我感觉，可能CPU能降低到5-10，能耗再降低很多
+				if counter == 2:
+					counter = 0
+				else:
+					x,y,c = frame.shape
+					# Flip the frame vertically
+					# frame = cv2.flip(frame, 1)
+					framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+					# Get hand landmark prediction
+					result = hands.process(framergb)
+					className = ''
+					landmarks = []
+
+					# post process the result
+					if result.multi_hand_landmarks:
+						for handslms in result.multi_hand_landmarks:
+							for lm in handslms.landmark:
+								lmx = int(lm.x*x)
+								lmy = int(lm.y*y)
+								landmarks.append([lmx,lmy])
+
+					# Predict gesture in Hand Gesture Recognition project
+					if landmarks:
+						prediction = model.predict([landmarks])
+						#print(prediction)
+						classID = np.argmax(prediction)
+						className = classNames[classID]
+						# show the prediction on the frame
+						cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2, cv2.LINE_AA)
+
+				# add the frame to the queue
+				self.Q.put(frame)
+				#print("fvs.update has been called and Q size is:"+str(self.Q.qsize()))
+			else:
+				print("the queue is full of data")
+			counter = counter +1
+
+	def read(self):
+		# return next frame in the queue
+		# print("fvs.read has been called and Q size is:"+str(self.Q.qsize()))
+		return self.Q.get()
+
+	def more(self):
+		# return True if there are still frames in the queue
+		return True
+
+	def stop(self):
+		# indicate that the thread should be stopped
+		print("thread stop method has been called")
+		self.stopped = True
+
+
 # initialize mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
@@ -41,45 +145,20 @@ f.close()
 print(classNames)
 
 # Initialize the webcam for Hand Gesture Recognition Python project
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
 
-#这段代码是这样的，第一次会失败，因为mac会问一下权限问题，第二次ok
-#按下q会结束程序
-while True:
-	# Read each frame from the webcam
-	_, frame = cap.read()
-	x,y,c = frame.shape
+#参考文章：https://lindevs.com/capture-rtsp-stream-from-ip-camera-using-opencv/
+#等于说就是换了下面那三句话而已
+RTSP_URL = 'rtsp://192.168.50.221:8554/live'
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;tcp'
+#cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
 
-	# Flip the frame vertically
-	frame = cv2.flip(frame, 1)
-	framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	# Get hand landmark prediction
-	result = hands.process(framergb)
+fvs = FileVideoStream(RTSP_URL).start()
+time.sleep(1.0)
 
-	className = ''
-
-	# post process the result
-	if result.multi_hand_landmarks:
-		landmarks = []
-		for handslms in result.multi_hand_landmarks:
-			for lm in handslms.landmark:
-				lmx = int(lm.x*x)
-				lmy = int(lm.y*y)
-				landmarks.append([lmx,lmy])
-			#这一步是画出来所有的手的特征点的
-			mpDraw.draw_landmarks(frame,handslms,mpHands.HAND_CONNECTIONS)
-		
-		# Predict gesture in Hand Gesture Recognition project
-		prediction = model.predict([landmarks])
-		print(prediction)
-		classID = np.argmax(prediction)
-		className = classNames[classID]
-		# show the prediction on the frame
-		cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2, cv2.LINE_AA)
+while fvs.more():
+	frame = fvs.read()
 	cv2.imshow("Output", frame)
 	if cv2.waitKey(1) == ord('q'):
-		break
-
-# release the webcam and destroy all active windows
-cap.release()
+ 		break
 cv2.destroyAllWindows()
